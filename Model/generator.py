42c0e413f0562092d2f747d4449c8925b80544ec
Samuel Snider-Held <samuel@mediamonks.com>
2020-11-25 15:44:17 -0500

    testing asymmetric attention layers

--- Model/generator.py
+++ Model/generator.py
@@ -20,9 +20,15 @@ def make_sagan_generator_model(img_dim, noise_shape, gen_kernel_size, kernel_ini
   
   generator = up_res_block(generator,num_filters // 4, gen_kernel_size, kernel_init)
 
-  generator = SelfAttention()(generator)
+  if(img_dim != 256):
+
+    generator = SelfAttention()(generator)
 
   generator = up_res_block(generator,num_filters // 8, gen_kernel_size, kernel_init)
+
+  if(img_dim == 256):
+
+    generator = SelfAttention()(generator)
 
   generator = up_res_block(generator,num_filters // 16, gen_kernel_size, kernel_init)
 
